e934ca7d9951bb111b2e7fc8e8c4f6d0f82e89c7
Cru5it0 <cruzisaacarandaervantes@gmail.com>
2023-07-20 12:40:15 -0600

    Project Spring Boot REST API Example

---
+++ src/test/java/org/bbva/example/SpringBootRestApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bbva.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
