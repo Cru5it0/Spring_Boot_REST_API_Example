155446631cb157a1d130333c1f3b82227cb2cac0
Cruz Isaac Aranda <62366109+Cru5it0@users.noreply.github.com>
2023-07-24 13:14:39 -0600

    Step 8. Demo

--- src/main/java/org/bbva/example/SpringBootRestApiExampleApplication.java
+++ src/main/java/org/bbva/example/SpringBootRestApiExampleApplication.java
@@ -1,13 +1,26 @@
 package org.bbva.example;
 
+import org.bbva.example.model.ItemModel;
+import org.bbva.example.model.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootRestApiExampleApplication {
+public class SpringBootRestApiExampleApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApiExampleApplication.class, args);
+	}
+
+	@Autowired
+	ItemRepository itemRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		itemRepository.save(new ItemModel(null, "Item 1"));
+		itemRepository.save(new ItemModel(null, "Item 2"));
 	}
 
 }
